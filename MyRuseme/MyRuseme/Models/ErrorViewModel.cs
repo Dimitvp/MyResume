971851f70fbed71cb415e7b9a1bddfc8dfdf3022
Dimitar <dimitar.nikolov.tvp@gmail.com>
2024-07-13 12:43:14 +0300

    Initial project

---
+++ MyRuseme/MyRuseme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyRuseme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
